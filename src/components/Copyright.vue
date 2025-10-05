f32dbd5bdcc49c00f21494af3c26f5068ca72b22
Moonlight4940 <alex4940@hotmail.com>
2025-10-05 22:31:58 +0200

    initial commit

---
+++ src/components/Copyright.vue
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<template>
+  <div class="copyright-info">
+    <p>
+        &copy; Copyright 2025 by HY Cho. All rights reserved.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
